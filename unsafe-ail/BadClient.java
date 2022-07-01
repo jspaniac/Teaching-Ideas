f373b6663feb074fbe51f5272abf9740bb088b88
jspaniac <jspaniac@cs.washington.edu>
2022-07-01 01:49:51 -0700

    Base for dummy password generator

---
+++ unsafe-ail/BadClient.java
@@ -0,0 +1,8 @@
+import sun.misc.unsafe;
+
+public BadClient {
+    public static void main (String[] args) {
+        ArrayIntList list = new ArrayIntList();
+        // TODO: Use unsafe to peek past the end of the 
+    }
+}
