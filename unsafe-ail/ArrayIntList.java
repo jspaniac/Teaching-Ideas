8bc31cbb0bcd50955a76a39d5f82c8ce297c45a9
jspaniac <jspaniac@cs.washington.edu>
2022-07-01 01:56:40 -0700

    Start of messing with AIL memory

--- unsafe-ail/ArrayIntList.java
+++ unsafe-ail/ArrayIntList.java
@@ -1,19 +1,22 @@
+// Class ArrayIntList can be used to store a list of integers.
+//
+// Smaller version than what we learned in class since we only need a few functions
+
 public class ArrayIntList {
    private int[] elementData;
    private int size;
-
+   
    public static final int DEFAULT_CAPACITY = 100;
    
+   // post: constructs an empty list of default capacity
    public ArrayIntList() {
       elementData = new int[DEFAULT_CAPACITY];
-      size = 0;
+      // TODO: Insert some values that you might not want someone else to see. 
+      //       Feel free to fully trust the safety of our implementation, we clear after! ;)
+      this.clear();
    }
    
-   public void add(int value) {
-      elementData[size] = value;
-      size++;
-   }
-   
+   // post: creates a comma-separated, bracketed version of the list
    public String toString() {
       if (size == 0) {
          return "[]";
@@ -25,5 +28,26 @@ public class ArrayIntList {
          result += "]";
          return result;
       }
-    }
+   }
+   
+   // pre : size() < capacity (throws IllegalStateException if not)
+   // post: appends the given value to the end of the list
+   public void add(int value) {
+      checkCapacity(size + 1);
+      elementData[size] = value;
+      size++;
+   }
+
+   // post: removes all values from the current list
+   public void clear() {
+      size = 0;
+   }
+   
+   // post: checks that the underlying array has the given capacity,
+   //       throwing an IllegalStateException if it does not
+   private void checkCapacity(int capacity) {
+      if (capacity > elementData.length) {
+         throw new IllegalStateException("would exceed list capacity");
+      }
+   }
 }
--- unsafe-ail/BadClient.java
+++ unsafe-ail/BadClient.java
@@ -3,6 +3,6 @@ import sun.misc.unsafe;
 public BadClient {
     public static void main (String[] args) {
         ArrayIntList list = new ArrayIntList();
-        // TODO: Use unsafe to peek past the end of the 
+        // TODO: Use unsafe to peek past at values past the end of the list
     }
 }
